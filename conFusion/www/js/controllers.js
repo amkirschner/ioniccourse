f59555e3ee6a21f7da54e9d3fd81f07455c7a770
Aaron Kirschner <aaron.kirschner@gmail.com>
2016-07-12 09:09:27 -0400

    Week 2 Assignment - After Step 2

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -173,8 +173,8 @@ angular.module('conFusion.controllers', [])
     }
 ])
 
-.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL',
-    function($scope, $stateParams, menuFactory, baseURL) {
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'favoriteFactory', '$ionicPopover', '$ionicListDelegate', 'baseURL',
+    function($scope, $stateParams, menuFactory, favoriteFactory, $ionicPopover, $ionicListDelegate, baseURL) {
 
         $scope.baseURL = baseURL;
         $scope.dish = {};
@@ -194,7 +194,36 @@ angular.module('conFusion.controllers', [])
                 }
         );
 
+          $ionicPopover.fromTemplateUrl('templates/dish-detail-popover.html', {
+            scope: $scope
+          }).then(function(popover) {
+            $scope.popover = popover;
+          });        
 
+          $scope.openPopover = function($event) {
+            $scope.popover.show($event);
+          };
+          $scope.closePopover = function() {
+            $scope.popover.hide();
+          };
+          //Cleanup the popover when we're done with it!
+          $scope.$on('$destroy', function() {
+            $scope.popover.remove();
+          });
+          // Execute action on hide popover
+          $scope.$on('popover.hidden', function() {
+            // Execute action
+          });
+          // Execute action on remove popover
+          $scope.$on('popover.removed', function() {
+            // Execute action
+          });
+
+                  $scope.addFavorites = function (index) {
+            console.log("index is " + index);
+            favoriteFactory.addToFavorites(index);
+            $ionicListDelegate.closeOptionButtons();
+        };
     }
 ])
 
